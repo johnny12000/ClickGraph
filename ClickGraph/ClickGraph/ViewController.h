daa629b3db6d7fade1e84d009c2c2f0b229ae8d9
Nikola Ristic <n.ristic@youngculture.com>
2015-08-03 10:49:28 +0200

    Initial commit

---
+++ ClickGraph/ClickGraph/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClickGraph
+//
+//  Created by nristic on 8/3/15.
+//  Copyright (c) 2015 Johnny12000. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ClickGraph/ClickGraph/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ClickGraph
+//
+//  Created by nristic on 8/3/15.
+//  Copyright (c) 2015 Johnny12000. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
